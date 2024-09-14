441a6e0acdfc6996a0eec4d1820c281b9390acfd
sarachafikidrissi <chafikidrissisara@gmail.com>
2024-09-14 12:49:24 +0100

    add sign up logic

--- src/context/index.js
+++ src/context/index.js
@@ -8,7 +8,15 @@ export const MyProvider = ({ children }) => {
 
     const [count, setCount] = useState(0)
 
-    const all = {count, setCount  }
+    const [userData, setUserData] = useState([])
+
+    const SignUp = (firstName, lastName, email, password) => {
+        const newUser = {firstName, lastName, email, password}
+
+        setUserData([...userData, newUser])
+    }
+
+    const all = {count, setCount, userData, setUserData, SignUp  }
 
     return (
         <Mycontext.Provider value={all}>{children}</Mycontext.Provider>
--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -1,44 +1,106 @@
-import React from 'react'
-import { useNavigate } from 'react-router-dom'
+import { React, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import { TiUser } from "react-icons/ti";
+import { useAuth } from "../../context";
 
 const SignUp = () => {
-    const navigate = useNavigate()
+  const navigate = useNavigate();
+
+  const { userData, SignUp } = useAuth();
+
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    SignUp(firstName, lastName, email, password);
+    setFirstName("")
+    setLastName("")
+    setEmail("")
+    setPassword("")
+    
+};
+console.log(userData);
+
+
+
+
   return (
-    <div className='flex sm:px-[10vw]  pb-[5vw] '>
-        <div className='sm:w-[60vw] w-full p-4 flex flex-col gap-y-3 '>
-        <h1 className='text-3xl'>Create Account</h1>
-        <span className='font-bold font-sans text-[#888888]'>Your Personal Details</span>
+    <div className="flex sm:px-[10vw]  pb-[5vw] ">
+      <div className="sm:w-[60vw] w-full p-4 flex flex-col gap-y-3 ">
+        <h1 className="text-3xl">Create Account</h1>
+        <span className="font-bold font-sans text-[#888888]">
+          Your Personal Details
+        </span>
 
-
-        <div className='flex flex-col gap-y-2'>
+        <div className="flex flex-col gap-y-2">
           <label htmlFor="">First Name</label>
-          <input type="text" className=' border border-[#b8b8b8] p-2' placeholder='First Name'/>
+          <input
+            type="text"
+            id="firstName"
+            value={firstName}
+            onChange={(e) => setFirstName(e.target.value)}
+            className=" border border-[#b8b8b8] p-2"
+            placeholder="First Name"
+          />
         </div>
-        <div className='flex flex-col gap-y-2'>
+        <div className="flex flex-col gap-y-2">
           <label htmlFor="">Last Name</label>
-          <input type="text" name="" id="" className=' border border-[#b8b8b8] p-2' placeholder='Last Name'/>
+          <input
+            type="text"
+            id="lastName"
+            value={lastName}
+            onChange={(e) => setLastName(e.target.value)}
+            className=" border border-[#b8b8b8] p-2"
+            placeholder="Last Name"
+          />
         </div>
 
-        <div className='flex flex-col gap-y-2'>
+        <div className="flex flex-col gap-y-2">
           <label htmlFor="">Email</label>
-          <input type="text" className=' border border-[#b8b8b8] p-2' placeholder='Email'/>
+          <input
+            type="text"
+            id="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            className=" border border-[#b8b8b8] p-2"
+            placeholder="Email"
+          />
         </div>
-        <div className='flex flex-col gap-y-2'>
+        <div className="flex flex-col gap-y-2">
           <label htmlFor="">Password</label>
-          <input type="password" name="" id="" className=' border border-[#b8b8b8] p-2' placeholder='Password'/>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            className=" border border-[#b8b8b8] p-2"
+            placeholder="Password"
+          />
           <span>Forgot Password?</span>
         </div>
-        <div className='flex gap-x-2 items-center'>
-        <button className='text-white bg-black px-6 py-1.5 w-fit flex items-center justify-center cursor-pointer'><TiUser className='text-xl' />CREATE</button>or
-        <span onClick={() => {navigate('/')}} className='hover:text-orange-600 cursor-pointer'> Return to Store</span>
+        <div className="flex gap-x-2 items-center">
+          <button onClick={handleSubmit} className="text-white bg-black px-6 py-1.5 w-fit flex items-center justify-center cursor-pointer">
+            <TiUser className="text-xl" />
+            CREATE
+          </button>
+          or
+          <span
+            onClick={() => {
+              navigate("/");
+            }}
+            className="hover:text-orange-600 cursor-pointer"
+          >
+            {" "}
+            Return to Store
+          </span>
         </div>
-
-
-
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default SignUp
+export default SignUp;
