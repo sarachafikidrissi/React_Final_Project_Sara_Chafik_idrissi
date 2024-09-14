44d035ec554da501ac38942d2b0292e63e5836fd
sarachafikidrissi <chafikidrissisara@gmail.com>
2024-09-14 12:57:45 +0100

    add login logic

--- src/context/index.js
+++ src/context/index.js
@@ -1,4 +1,5 @@
 import { createContext, useState, useContext } from "react";
+import { useNavigate } from "react-router-dom";
 
 
 export const Mycontext = createContext()
@@ -9,6 +10,8 @@ export const MyProvider = ({ children }) => {
     const [count, setCount] = useState(0)
 
     const [userData, setUserData] = useState([])
+    const [currentUser, setCurrentUser] = useState(null);  
+    const navigate = useNavigate();
 
     const SignUp = (firstName, lastName, email, password) => {
         const newUser = {firstName, lastName, email, password}
@@ -16,7 +19,18 @@ export const MyProvider = ({ children }) => {
         setUserData([...userData, newUser])
     }
 
-    const all = {count, setCount, userData, setUserData, SignUp  }
+    const login = (email, password) => {
+        const user = userData.find((user) => user.email === email && user.password === password);
+
+        if (user) {
+            setCurrentUser(user);
+            navigate('/'); // Navigate after successful login
+        } else {
+            alert('Invalid email or password');
+        }
+    };
+
+    const all = {count, setCount, userData, setUserData, SignUp, login  }
 
     return (
         <Mycontext.Provider value={all}>{children}</Mycontext.Provider>
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,42 +1,93 @@
-import React from 'react'
-import { useNavigate } from 'react-router-dom'
+import {React, useState} from "react";
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "../../context";
 
 const Login = () => {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
+  const { login } = useAuth();
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+
+    login(email, password);
+  };
+
   return (
-    <div className=' flex sm:flex-row flex-col gap-y-4 justify-center gap-x-4 py-10 bg-red-500sm:px-0 px-2 '>
-      <div className='sm:w-[40vw]  border border-[#d3d3d3] p-4 flex flex-col gap-y-[5vh] h-fit'>
+    <div className=" flex sm:flex-row flex-col gap-y-4 justify-center gap-x-4 py-10 bg-red-500sm:px-0 px-2 ">
+      <div className="sm:w-[40vw]  border border-[#d3d3d3] p-4 flex flex-col gap-y-[5vh] h-fit">
         <div>
-          <h1 className='text-3xl'>New Customer</h1>
-        <span className='font-bold font-sans text-[#888888]'>Register Account</span>
-        <p className='text-[#888888]'>By creating an account you will be able to shop faster, be up to date on an order's status, and keep track of the orders you have previously made.</p>
+          <h1 className="text-3xl">New Customer</h1>
+          <span className="font-bold font-sans text-[#888888]">
+            Register Account
+          </span>
+          <p className="text-[#888888]">
+            By creating an account you will be able to shop faster, be up to
+            date on an order's status, and keep track of the orders you have
+            previously made.
+          </p>
         </div>
 
-        <button onClick={() => {navigate('/register')}} className='text-white bg-black px-6 py-1.5 w-fit'>CONTINUE</button>
+        <button
+          onClick={() => {
+            navigate("/register");
+          }}
+          className="text-white bg-black px-6 py-1.5 w-fit"
+        >
+          CONTINUE
+        </button>
       </div>
-      <div className='sm:w-[40vw] border border-[#d3d3d3] p-4 flex flex-col gap-y-3 '>
-        <h1 className='text-3xl'>Returning Customer</h1>
-        <span className='font-bold font-sans text-[#888888]'>I am a returning customer</span>
+      <div className="sm:w-[40vw] border border-[#d3d3d3] p-4 flex flex-col gap-y-3 ">
+        <h1 className="text-3xl">Returning Customer</h1>
+        <span className="font-bold font-sans text-[#888888]">
+          I am a returning customer
+        </span>
 
-        <div className='flex flex-col gap-y-2'>
+        <div className="flex flex-col gap-y-2">
           <label htmlFor="">Email</label>
-          <input type="text" className=' border border-[#b8b8b8] p-2' placeholder='Email'/>
+          <input
+            id="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+            type="text"
+            className=" border border-[#b8b8b8] p-2"
+            placeholder="Email"
+          />
         </div>
-        <div className='flex flex-col gap-y-2'>
+        <div className="flex flex-col gap-y-2">
           <label htmlFor="">Password</label>
-          <input type="password" name="" id="" className=' border border-[#b8b8b8] p-2' placeholder='Password'/>
+          <input
+            type="password"
+            id='password'
+            className='border border-[#b8b8b8] p-2'
+            placeholder='Password'
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
           <span>Forgot Password?</span>
         </div>
-        <div className='flex gap-x-2'>
-        <button className='text-white bg-black px-6 py-1.5 w-fit cursor-pointer'>SIGN IN</button>or
-        <span onClick={() => {navigate('/')}} className='hover:text-orange-600 cursor-pointer'> Return to Store</span>
+        <div className="flex gap-x-2">
+          <button onClick={handleLogin} type="submit" className="text-white bg-black px-6 py-1.5 w-fit cursor-pointer">
+            SIGN IN
+          </button>
+          or
+          <span
+            onClick={() => {
+              navigate("/");
+            }}
+            className="hover:text-orange-600 cursor-pointer"
+          >
+            {" "}
+            Return to Store
+          </span>
         </div>
-
-
-
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
